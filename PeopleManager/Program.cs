5fd4eb0b852c414fdd4b520fd62b896f34058e7f
GorettiSuarez <goretti__sr.95@hotmail.es>
2021-09-27 19:41:32 +0100

    Agregar archivos de proyecto.

---
+++ PeopleManager/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PeopleManager
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
